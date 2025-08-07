a87bd9b86807ba34bdfacd73552527e745e8cc56
Oleksii Skrebtsov <alex.skrebtsov1213@gmail.com>
2025-08-07 11:55:42 +0000

    base setup

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+    return (
+        <div className="w-full flex flex-col items-center gap-20 py-20 md:py-12 md:gap-12">
+            page
+        </div>
+    );
+}
