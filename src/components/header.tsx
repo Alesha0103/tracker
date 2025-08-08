3c888217e38fff3b6efb29cb3174a082264f08fe
Oleksii Skrebtsov <alex.skrebtsov1213@gmail.com>
2025-08-08 07:29:29 +0000

    base translations

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,33 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 
 import Logo from "../assets/images/logo.png";
 import { useTranslations } from "next-intl";
+import { useAuthStore } from "@/store/auth-store";
+import { Locale } from "@/enums/auth";
 
 export const Header = () => {
     const t = useTranslations("general");
+    const { setLocale } = useAuthStore();
+
     return (
         <div className="border-b-2 border-white/10 py-2 px-6">
             <div className="flex gap-x-2 items-center">
                 <Image width={25} height={25} src={Logo} alt="logo" priority />
                 <span className="text-white font-fantasy">{t("appName")}</span>
             </div>
+
+            <button onClick={() => setLocale(Locale.UA)} className="bg-white">
+                Change Locale to UA
+            </button>
+            <button
+                onClick={() => setLocale(Locale.EN)}
+                className="bg-white ml-10"
+            >
+                Change Locale to En
+            </button>
         </div>
     );
 };
---
+++ src/store/auth-store.ts
@@ -0,0 +1,28 @@
+import { Locale } from "@/enums/auth";
+import { setCookie } from "cookies-next";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface Authtate {
+    locale: Locale;
+    setLocale: (locale: Locale) => void;
+}
+
+export const useAuthStore = create<Authtate>()(
+    persist(
+        (set) => ({
+            locale: Locale.EN,
+
+            setLocale: (locale: Locale) => {
+                set({ locale });
+                setCookie("locale", locale);
+            },
+        }),
+        {
+            name: "auth",
+            partialize: (state) => ({
+                locale: state.locale,
+            }),
+        }
+    )
+);
