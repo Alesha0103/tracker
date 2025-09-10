bc5570e33aca5d46cf606a9c49f1f118d7a48dc9
Oleksii Skrebtsov <alex.skrebtsov1213@gmail.com>
2025-09-10 13:29:00 +0000

    new access flow

--- src/services/auth/endpoints.ts
+++ src/services/auth/endpoints.ts
@@ -1,9 +1,14 @@
-import { AuthResponse, SignInDto } from "@/types/auth";
+import { SignInDto } from "@/types/auth";
 import { api } from "../../configs/api";
+import { User } from "@/types/users";
 
-export const signIn = async (dto: SignInDto): Promise<AuthResponse> => {
+export const signIn = async (dto: SignInDto): Promise<User> => {
     const { data } = await api.post("/login", dto, { withCredentials: true });
     return data;
+};
+
+export const refresh = async () => {
+    await api.get("/refresh", { withCredentials: true });
 };
 
 export const logout = async () => {
